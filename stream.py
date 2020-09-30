d9d48e22f2c0f36980b36b4344303820c3d982d4
sean <seanpollock3344@gmail.com>
2020-09-30 17:02:34 -0500

    laplacian of frame and calc mean

--- stream.py
+++ stream.py
@@ -1,4 +1,5 @@
 import cv2
+from controls.Verifocal import Focuser
 
 if __name__ == '__main__':
     pipeline = (
@@ -10,7 +11,13 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
     if cap.isOpened():
         while True:
-            window = cv2.namedWindow("Camera")
+            cv2.namedWindow("Raw", cv2.WINDOW_AUTOSIZE)
+            cv2.namedWindow("Proc")
             _, frame = cap.read()
-            cv2.imshow("Camera", frame)
+            proc = frame.copy()
+            proc = cv2.Laplacian(frame, cv2.CV_64F)
+            score = cv2.mean(proc)[0]
+            print('proc score', score)
+            cv2.imshow("Raw", frame)
+            cv2.imshow("Proc", proc)
             cv2.waitKey(1)
