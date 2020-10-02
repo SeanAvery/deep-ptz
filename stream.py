ebc52a8e1564fcbdc0bba68815d92f771038ee0a
sean <seanpollock3344@gmail.com>
2020-10-01 22:18:13 -0500

    wip sobel focuser

--- stream.py
+++ stream.py
@@ -11,6 +11,12 @@ if __name__ == '__main__':
     env = Manager()
     cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
     if cap.isOpened():
+        prev_score = 0
+        moves = {
+            'in': 3,
+            'out': 4,
+        }
+        move = 'in'
         while True:
             cv2.namedWindow("Raw", cv2.WINDOW_AUTOSIZE)
             cv2.namedWindow("Proc")
@@ -18,10 +24,18 @@ if __name__ == '__main__':
             proc = frame.copy()
             proc = cv2.Laplacian(frame, cv2.CV_64F)
             score = cv2.mean(proc)[0]
-            cmd = env.choose_action()
-            print('cmd', cmd)
-            env.move(cmd)
             print('proc score', score)
+            diff = score - prev_score
+            print('diff', diff)
+            if diff >= 0:
+                env.move(move)
+            if diff < 0:
+                if move == 'in':
+                    move = 'out'
+                    env.move(moves[move])
+                if move == 'out':
+                    move = 'in'
+                    env.move(moves[move])
             cv2.imshow("Raw", frame)
             cv2.imshow("Proc", proc)
             cv2.waitKey(1)
